d3479cececa345a68f3a3fbd373a539217ffbde0
samchi <seyoung0314@gmail.com>
2025-02-05 10:43:54 +0900

    폼 취소버튼 토글

--- src/components/new-expense/ExpenseForm.jsx
+++ src/components/new-expense/ExpenseForm.jsx
@@ -1,17 +1,15 @@
+import "./ExpenseForm.css";
+import React, { useState } from "react";
 
-import './ExpenseForm.css';
-import React, { useState } from 'react';
-
-const ExpenseForm = ({ onAdd }) => {
-
+const ExpenseForm = ({ onAdd, onCancle }) => {
   // 단일 상태값 관리
   // const [title, setTitle] = useState('');
   // const [price, setPrice] = useState(0);
   // const [date, setDate] = useState(null);
   const initialUserInput = {
-    title: '',
+    title: "",
     price: 0,
-    date: '',
+    date: "",
   };
   // 객체 상태값 관리
   const [userInput, setUserInput] = useState(initialUserInput);
@@ -20,13 +18,13 @@ const ExpenseForm = ({ onAdd }) => {
   const getTodayDate = () => {
     const today = new Date();
     const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 1을 더해줌
-    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 1을 더해줌
+    const day = String(today.getDate()).padStart(2, "0");
     return `${year}-${month}-${day}`;
   };
 
   // form submit 이벤트
-  const handleSubmit = e => { 
+  const handleSubmit = (e) => {
     e.preventDefault();
     // console.log('submit!!');
     // const payload = {
@@ -35,28 +33,24 @@ const ExpenseForm = ({ onAdd }) => {
     //   date
     // };
 
-    console.log('payload: ', userInput);
-    
+    console.log("payload: ", userInput);
+
     // 부모가 보낸 데이터전달용 함수를 호출
     onAdd({
       ...userInput,
-      date: new Date(userInput.date)
+      date: new Date(userInput.date),
     });
-    
-
   };
 
-  const handleTitleInput = e => { 
+  const handleTitleInput = (e) => {
     // 리액트는 상태값변경은 반드시 setter를 통해서 수행
     // 상태값이 객체나 배열일 경우에는 항상 새로운 객체, 배열을 세팅하라
-    setUserInput((prevState) =>  
-      ({
-       ...prevState,
-       title: e.target.value,
-       })
-   );
+    setUserInput((prevState) => ({
+      ...prevState,
+      title: e.target.value,
+    }));
   };
-  const handlePriceInput = (e) => { 
+  const handlePriceInput = (e) => {
     setUserInput({
       ...userInput,
       price: +e.target.value,
@@ -71,38 +65,42 @@ const ExpenseForm = ({ onAdd }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className='new-expense__controls'>
-        <div className='new-expense__control'>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
           <label>Title</label>
           <input
-            type='text'
+            type="text"
             onInput={handleTitleInput}
             value={userInput.title}
           />
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Price</label>
           <input
-            type='number'
-            min='100'
-            step='100'
+            type="number"
+            min="100"
+            step="100"
             onInput={handlePriceInput}
             value={userInput.price}
           />
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Date</label>
           <input
-            type='date'
-            min='2019-01-01'
+            type="date"
+            min="2019-01-01"
             max={getTodayDate()}
             onInput={handleDateInput}
             value={userInput.date}
           />
         </div>
       </div>
-      <div className='new-expense__actions'>
-        <button type='submit'>Add Expense</button>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+        <button type="button" onClick={onCancle}>
+          {" "}
+          취소{" "}
+        </button>
       </div>
     </form>
   );
--- src/components/new-expense/NewExpense.jsx
+++ src/components/new-expense/NewExpense.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 const NewExpense = ({onSave}) => {
+
+  //새로운 지출 버튼을 눌렀는지 여부를 상태관리
+  const [toggle,setToggle] =useState(false);
+
+  let content = <button onClick={()=>setToggle(true)}>새로운 지출 추가하기</button>;
+
+  if(toggle){
+    content = <ExpenseForm onAdd={onSave} onCancle={()=>setToggle(false)} />;
+  }
+
   return (
     <div className='new-expense'>
-      <ExpenseForm onAdd={onSave} />
+      {content}
     </div>
   );
 };
