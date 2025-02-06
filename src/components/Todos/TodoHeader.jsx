2b6a34e23197153777537e0d091fbbdd31d7175a
samchi <seyoung0314@gmail.com>
2025-02-06 16:13:10 +0900

    todo list - 오늘 날짜 반영

--- src/components/Todos/TodoHeader.jsx
+++ src/components/Todos/TodoHeader.jsx
@@ -5,10 +5,15 @@ import TodoTemplet from './TodoTemplate';
 export const TodoHeader = ({count}) => {
 
   const { day, 'tasks-left': left } = styles;
+  
+  const date = new Date();
+  const today = `${date.getFullYear()}년 ${date.getMonth()+1}월 ${date.getDate()}일`;
+  const week = `${date.toLocaleString('ko-KR', { weekday: 'long' })}`;
+  
   return (
     <header>
-      <h1>2025년 2월 6일</h1>
-      <div className={day}>목요일</div>
+      <h1>{today}</h1>
+      <div className={day}>{week}</div>
       <div className={left}>할 일 {count}개 남음</div>
     </header>
   );
