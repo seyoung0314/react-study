a5a1ae1e5fe1de0d708b77756a2a31ee17d4d12d
samchi <seyoung0314@gmail.com>
2025-02-07 18:04:47 +0900

    useEffect안쓰고 sideEffect 막아봄

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,31 @@ import Login from "./components/SideEffect/Login";
 // 컴포넌트
 const App = () => {
   // 로그인 상태 체크
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    // 원래 백에서 로그인 토큰 관리하는거니까 
+    // 여기다가 억지로 fetch 날릴수잇음
+    localStorage.getItem("login-flag") === "1"
+  );
 
-  
-  
   // 로그인 상태 여부 파악 (localStorage에 토큰 유무 확인)
-  const token = localStorage.getItem('login-flag');
-
+  const token = localStorage.getItem("login-flag");
 
   // 리액트는 기본적으로 상태값이 변경(다른값으로)되었을때만 감지하는데
-  // 컴포넌트가 렌더링중일 때 setState 했을 때 
+  // 컴포넌트가 렌더링중일 때 setState 했을 때
   // 상태값이 변경가능성이 있다고 판단해서 리렌더링을 처리
-  // if(token !== "1"){
-  //   setIsLoggedIn(false);
+
+  // if(token === "1"){
+  //   setIsLoggedIn(true);
   // }
-  
+
   const handleLogin = (email, password) => {
     console.log(isLoggedIn);
     //검증
     if (email === "aaa@aaa.com" && password === "1234567") {
-      if(token !== "1"){
-        localStorage.setItem("login-flag", "1");
-      }
+      if(token !== 1){
+      localStorage.setItem("login-flag", "1");
       setIsLoggedIn(true);
+      }
     } else {
       alert("실패");
     }
