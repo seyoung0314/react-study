6a85368a91da76eba35cfcbb828e911e5073d0aa
samchi <seyoung0314@gmail.com>
2025-02-07 14:01:34 +0900

    side effect 발생

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,49 @@
-import { useState } from 'react';
-import './App.css';
-import MainHeader from './components/SideEffect/MainHeader';
-import Home from './components/SideEffect/Home';
-import Login from './components/SideEffect/Login';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import MainHeader from "./components/SideEffect/MainHeader";
+import Home from "./components/SideEffect/Home";
+import Login from "./components/SideEffect/Login";
 
 // 컴포넌트
 const App = () => {
+  // 로그인 상태 체크
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  
+  
+  // 로그인 상태 여부 파악 (localStorage에 토큰 유무 확인)
+  const token = localStorage.getItem('login-flag');
 
 
+  // 리액트는 기본적으로 상태값이 변경(다른값으로)되었을때만 감지하는데
+  // 컴포넌트가 렌더링중일 때 setState 했을 때 
+  // 상태값이 변경가능성이 있다고 판단해서 리렌더링을 처리
+  // if(token !== "1"){
+  //   setIsLoggedIn(false);
+  // }
+  
+  const handleLogin = (email, password) => {
+    console.log(isLoggedIn);
+    //검증
+    if (email === "aaa@aaa.com" && password === "1234567") {
+      if(token !== "1"){
+        localStorage.setItem("login-flag", "1");
+      }
+      setIsLoggedIn(true);
+    } else {
+      alert("실패");
+    }
+  };
 
-  return <>
-<MainHeader/>
-<main>
-  <Home/>
-  <Login/>
-</main>
-</>
+  return (
+    <>
+      <MainHeader />
+      <main>
+        {isLoggedIn && <Home />}
+        {!isLoggedIn && <Login onLogin={handleLogin} />}
+      </main>
+    </>
+  );
 };
 
 export default App;
--- src/components/SideEffect/Login.jsx
+++ src/components/SideEffect/Login.jsx
@@ -1,34 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Card from '../ui/Card';
-import styles from './Login.module.css';
-import Button from '../ui/Button';
+import Card from "../ui/Card";
+import styles from "./Login.module.css";
+import Button from "../ui/Button";
 
-const Login = () => {
-  
+const Login = ({onLogin}) => {
+  //사용자가 입력한 이메일 상태변수
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+
+  //검증용 상태변수
+  // 초기값을 true로 세팅해놓으면 초기화면에서 검증이 뚫리기때문에
+  // falsy한 값으로 설정해두고(null,'',...) 명확히 false만 클래스 붙도록
+  const [emailIsValid, setEmailIsValid] = useState();
+  const [passwordIsValid, setPasswordIsValid] = useState();
+
+  // 로그인버튼 상태변수
+  const [formIsValid , setFormIsValid] = useState(false);
+
+  const handleEmail = (e) => {
+    setEnteredEmail(e.target.value);
+
+    setFormIsValid(
+      enteredEmail.includes('@') && e.target.value.trim().length > 6
+    );
+  };
+
+  const handlePassword = (e) => {
+    setEnteredPassword(e.target.value);
+
+    setFormIsValid(
+      enteredEmail.includes('@') && e.target.value.trim().length > 6
+    );
+  };
+
+  // 이메일 검증
+  const vaildateEmail = (e) => {
+    setEmailIsValid(enteredEmail.includes("@"));
+  };
+
+  // 비밀번호 검증
+  const vaildatePassword = (e) => {
+    setPasswordIsValid(enteredPassword.trim().length > 6);
+  };
+
+  //로그인 수행
+  const handleSubmit = e =>{
+    e.preventDefault();
+    onLogin(enteredEmail,enteredPassword);
+  }
+
   return (
     <Card className={styles.login}>
-      <form>
-        <div
-          className={`${styles.control}`}>
-          <label htmlFor='email'>E-Mail</label>
+      <form onSubmit={handleSubmit}>
+        <div className={`${styles.control} ${emailIsValid === false? styles.invalid : ''}`}>
+          <label htmlFor="email">E-Mail</label>
           <input
-            type='email'
-            id='email'
+            type="email"
+            id="email"
+            value={enteredEmail}
+            onInput={handleEmail}
+            onBlur={vaildateEmail}
           />
         </div>
-        <div
-          className={`${styles.control}`}>
-          <label htmlFor='password'>Password</label>
+        <div className={`${styles.control} ${passwordIsValid === false? styles.invalid : ''}`}>
+          <label htmlFor="password">Password</label>
           <input
-            type='password'
-            id='password'
+            type="password"
+            id="password"
+            value={enteredPassword}
+            onInput={handlePassword}
+            onBlur={vaildatePassword}
           />
         </div>
         <div className={styles.actions}>
-          <Button
-            type='submit'
-            className={styles.btn}
+          <Button 
+          type="submit" 
+          className={styles.btn}
+          disabled={!formIsValid}
           >
             Login
           </Button>
