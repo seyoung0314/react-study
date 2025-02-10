68244a8e67edb8d65c649c2ac5672d42c5881a53
samchi <seyoung0314@gmail.com>
2025-02-10 10:36:02 +0900

    (복습) 유저이름 변경처리

--- src/components/TimerGame/Player.jsx
+++ src/components/TimerGame/Player.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+
 const Player = () => {
+
+  // input태그를 저장할 ref
+  const nameRef = useRef();
+
+  const [enteredName, setEnteredName] = useState('anonymous');
+
+  const handleChangeName = e => { 
+    
+    const $input = nameRef.current;
+    setEnteredName($input.value);
+
+    $input.value = '';
+  };
+
   return (
     <section id='player'>
-      <h2>Welcome anonymous!</h2>
+      <h2>Welcome { enteredName }!</h2>
       <p>
-        <input type='text' />
-        <button>Set Name</button>
+        <input type='text' ref={nameRef} />
+        <button onClick={handleChangeName}>Set Name</button>
       </p>
     </section>
   );
 };
+
 export default Player;
