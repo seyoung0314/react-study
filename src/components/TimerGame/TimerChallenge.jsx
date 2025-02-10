a613da4a2471f01fec34e2bf7edbb979eeea12e0
samchi <seyoung0314@gmail.com>
2025-02-10 10:37:32 +0900

    타이머가 멈추지 않는 이유

--- src/components/TimerGame/TimerChallenge.jsx
+++ src/components/TimerGame/TimerChallenge.jsx
@@ -1,17 +1,46 @@
 import React, { useState } from 'react';
+
 const TimerChallenge = ({ title, targetTime }) => {
+
   // 타이머가 시작되었는지를 확인하는 상태값
   const [timerStarted, setTimerStarted] = useState(false);
+
   // 시간이 다 지났는지 여부
   const [timerExpired, setTimerExpired] = useState(false);
-  const handleStart = e => { 
+
+
+  // 타이머 id 저장
+  let timer;
+
+  // start 이벤트
+  const handleStart = e => {
     setTimerStarted(true);
+
+    /*
+      setTimeout실행시 타이머의 id(aaa)가 생성된다.
+      타이머 실행시 setTimerExpired가 실행됨.
+
+      상태변수는 실행시 컴포넌트를 리렌더링함.
+      리렌더링할 때 기존에 저장한 timer id(aaa)를 없앤다.
+
+      따라서 제대로 clear를 수행하지 못함
+    */
+
     // 실제 시간을 실행
-    setTimeout(() => {
-      // console.log(`${targetTime} 초가 지남!`);
+    timer = setTimeout(() => {
+      console.log(`${targetTime} 초가 지남!`);
       setTimerExpired(true);
     }, targetTime * 1000);
+
+    console.log(`start timer: ${timer}`);
   };
+
+  // stop 이벤트
+  const handleStop = e => { 
+    console.log('타이머를 중지함! stop timer : ', timer);
+    clearTimeout(timer); // 타이머 해제
+  };
+
   return (
     <section className='challenge'>
       <h2>{title}</h2>
@@ -20,7 +49,7 @@ const TimerChallenge = ({ title, targetTime }) => {
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? 'Stop' : 'Start'} Challenge
         </button>
       </p>
@@ -30,4 +59,5 @@ const TimerChallenge = ({ title, targetTime }) => {
     </section>
   );
 };
+
 export default TimerChallenge;
