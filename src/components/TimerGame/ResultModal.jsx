1dd611a66dc966788dfc83bc74141d52226da231
samchi <seyoung0314@gmail.com>
2025-02-11 10:38:17 +0900

    점수추가

--- src/components/TimerGame/ResultModal.jsx
+++ src/components/TimerGame/ResultModal.jsx
@@ -11,14 +11,19 @@ const ResultModalComponent = ({ result, targetTime, timeRemaining, onReset },  r
   // 남은시간을 밀리초에서 초로 변환
   const formattedTime = (timeRemaining / 1000).toFixed(2);
 
+    // 점수
+    const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
+
   return (
     <dialog ref={ref} className='result-modal'>
       <h2>Your {result}!</h2>
+      {result === 'won' && <h2>Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-      You stopped the timer with <strong>{ formattedTime } seconds left.</strong>
+      You stopped the timer with{' '}
+        <strong>{formattedTime} seconds left.</strong>
       </p>
       <form method='dialog'>
         <button>Close</button>
